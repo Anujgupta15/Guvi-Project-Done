e30d7cce68cc478d0b01137490f6642939f8f331
Anuj Gupta <anujgupt15@gmail.com>
2021-12-21 18:10:29 +0530

    Second

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,36 @@
+const dotenv = require("dotenv");
+
 const express = require("express");
 const notes = require("./data/notes");
-const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+
+dotenv.config({ path: "../.env" });
+const path = require("path");
+
+// console.log(process.env.PORT);
+
+require("./config/db");
+// const DB = process.env.MONGO_URI;
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => {
+//     console.log(`Connection successful`);
+//   })
+//   .catch((err) => console.log(`no connection`));
 
 const app = express();
 
-dotenv.config();
+// connectDB();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
@@ -14,11 +40,23 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
+app.use("/api/users", userRoutes);
 
-  res.send(note);
-});
+app.use(notFound);
+app.use(errorHandler);
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Running on PORT ${PORT}`));
